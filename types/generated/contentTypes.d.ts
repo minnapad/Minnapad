ade0c3119effc5baba52e89d2c88b5d7790b8561
Mouneeswar <minnapadv2@gmail.com>
2024-07-04 15:25:06 +0530

    Revert "changes in  homepage"
    This reverts commit 4d8944d88ceada944d98196161c062afc82b3fcc.

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -929,7 +929,7 @@ export interface ApiHomepageHomepage extends Schema.SingleType {
       true
     >;
     legends: Attribute.Component<'legends.legends', true>;
-    joinProjectSteps: Attribute.Component<'home-how-to-join-projects.home-how-to-join'>;
+    howToJoinProjects: Attribute.Component<'home-how-to-join-projects.home-how-to-join'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -1008,19 +1008,7 @@ export interface ApiLegendLegend extends Schema.CollectionType {
     draftAndPublish: true;
   };
   attributes: {
-    name: Attribute.String & Attribute.Required;
-    role: Attribute.String & Attribute.Required;
-    profilePicture: Attribute.String;
-    knownFor: Attribute.String;
-    isUpcoming: Attribute.Boolean;
-    upcomingDate: Attribute.Date;
-    projects: Attribute.Relation<
-      'api::legend.legend',
-      'manyToMany',
-      'api::project.project'
-    >;
-    bio: Attribute.RichText;
-    portfolioBanner: Attribute.Media<'images'>;
+    Legend: Attribute.Component<'legends.legends', true>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -1062,45 +1050,6 @@ export interface ApiPrivacyPolicyPrivacyPolicy extends Schema.SingleType {
       Attribute.Private;
     updatedBy: Attribute.Relation<
       'api::privacy-policy.privacy-policy',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiProjectProject extends Schema.CollectionType {
-  collectionName: 'projects';
-  info: {
-    singularName: 'project';
-    pluralName: 'projects';
-    displayName: 'projects';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    name: Attribute.String;
-    banner: Attribute.Media<'images'>;
-    logo: Attribute.Media<'images'>;
-    legends: Attribute.Relation<
-      'api::project.project',
-      'manyToMany',
-      'api::legend.legend'
-    >;
-    description: Attribute.RichText;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::project.project',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::project.project',
       'oneToOne',
       'admin::user'
     > &
@@ -1283,7 +1232,6 @@ declare module '@strapi/types' {
       'api::keiji.keiji': ApiKeijiKeiji;
       'api::legend.legend': ApiLegendLegend;
       'api::privacy-policy.privacy-policy': ApiPrivacyPolicyPrivacyPolicy;
-      'api::project.project': ApiProjectProject;
       'api::seichi-inafune.seichi-inafune': ApiSeichiInafuneSeichiInafune;
       'api::support.support': ApiSupportSupport;
       'api::terms-and-conditions.terms-and-conditions': ApiTermsAndConditionsTermsAndConditions;
